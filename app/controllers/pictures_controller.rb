65fcc5ab5b2b4b4ce5ebf0086d521adb212f57b2
Celia Dang <dang.celia@gmail.com>
2014-01-28 15:28:23 -0500

    Adding feature to edit pictures

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,13 @@
 class PicturesController < ApplicationController
+	def index
+		@pictures = Picture.all
+
+	end
+
+	def show
+		@picture = Picture.find(params[:id])
+
+	end
 	def new
 		@picture = Picture.new
 
@@ -15,8 +24,22 @@ class PicturesController < ApplicationController
 		end
 	end
 
+	def edit
+		@picture = Picture.find(params[:id])
+
+		if @picture.update_attributes(picture_params)
+			redirect_to "/pictures/#{@picture.id}"
+
+		else
+			render :edit
+
+		end
+
+	end
+
 	private
 	def picture_params
 		params.require(:picture).permit(:artist, :title, :url)
 	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Photogur::Application.routes.draw do
   root :to => "pictures#index"
   get 'pictures' => 'pictures#index'
 
+  get 'pictures/:id/edit' => "pictures#edit", as: "edit_picture"
+  patch 'pictures/:id' => "pictures#update"
+
   post 'pictures' => "pictures#create"
   get 'pictures/new' => 'pictures#new'
 
