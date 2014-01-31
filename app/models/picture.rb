f3d90310033cbd3d6a757c5abfb844f9d3aa86f1
Celia Dang <dang.celia@gmail.com>
2014-01-31 16:47:26 -0500

    Display most recent pictures on the index page

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,2 +1,5 @@
 class Picture < ActiveRecord::Base
+	scope :newest_first, -> {order("created_at DESC")}
+	scope :most_recent_five, -> {order("created_at DESC").limit(5)}
+	scope :created_before, ->(time) {where("created_at < ?", time)}
 end
